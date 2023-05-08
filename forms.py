2848a3a7f67e3602a2dd9337554af22d3fffddd5
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-05-08 14:45:07 +0200

    Centered the login form

--- forms.py
+++ forms.py
@@ -5,5 +5,4 @@ from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()], render_kw={"type": "password"})
-
     submit = SubmitField('Submit')
