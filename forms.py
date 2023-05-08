5c988b64b4b1ca7db30933144a947ee91014b475
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-05-08 15:20:55 +0200

    Changed login field classes

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('', validators=[DataRequired()], render_kw={"placeholder": "Username",
-                                                                       "class": "form-control"})
+                                                                       "class": "user"})
     password = StringField('', validators=[DataRequired()], render_kw={"type": "password", "placeholder": "Password",
-                                                                       "class": "form-control"})
+                                                                       "class": "pass"})
     submit = SubmitField('Submit', render_kw={"class": "btn btn-primary"})
