98900f62e7f9db2ccd9f03cff0f7b1c05c47f2e4
DBerkey <DAW.berkeij@student.han.nl>
2023-05-17 13:13:32 +0200

    fixed non disepearing info windows

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,35 +1,35 @@
-function infoPopUp_orgname() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
+function infoPopUp_orgname() {hide_protname();hide_header();hide_seq();hide_eval_threshold();
     hide_query_coverage();hide_origin();
     var popup = document.getElementById("infopopup_orgname");
     popup.classList.toggle("show");}
 
-function infoPopUp_protname() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
+function infoPopUp_protname() {hide_orgname();hide_header();hide_seq();hide_eval_threshold();
     hide_query_coverage();hide_origin();
     var popup = document.getElementById("infopopup_protname");
     popup.classList.toggle("show");}
 
-function infoPopUp_header() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
+function infoPopUp_header() {hide_orgname();hide_protname();hide_seq();hide_eval_threshold();
     hide_query_coverage();hide_origin();
     var popup = document.getElementById("infopopup_header");
     popup.classList.toggle("show");}
 
-function infoPopUp_seq() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
+function infoPopUp_seq() {hide_orgname();hide_protname();hide_header();hide_eval_threshold();
     hide_query_coverage();hide_origin();
     var popup = document.getElementById("infopopup_seq");
     popup.classList.toggle("show");}
 
-function infoPopUp_eval_threshold() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
+function infoPopUp_eval_threshold() {hide_orgname();hide_protname();hide_header();hide_seq();
     hide_query_coverage();hide_origin();
     var popup = document.getElementById("infopopup_eval_threshold");
     popup.classList.toggle("show");}
 
 function infoPopUp_query_coverage() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
-    hide_query_coverage();hide_origin();
+    hide_origin();
     var popup = document.getElementById("infopopup_query_coverage");
     popup.classList.toggle("show");}
 
 function infoPopUp_origin() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
-    hide_query_coverage();hide_origin();
+    hide_query_coverage();
     var popup = document.getElementById("infopopup_origin");
     popup.classList.toggle("show");}
 
