32f921df07945fff7f5b66bc0b1f9301d0264897
DBerkey <DAW.berkeij@student.han.nl>
2023-05-17 10:06:09 +0200

    all info buttons

--- forms.py
+++ forms.py
@@ -46,14 +46,34 @@ class Login(FlaskForm):
 
 
 class Search(FlaskForm):
-    orgname = StringField(Markup('<button_orgname class="info-button", onclick="infoPopUp()", title="info">'
+    orgname = StringField(Markup('<button_orgname class="info-button", onclick="infoPopUp_orgname()", title="info">'
                                  '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
-                                 '<span class="popuptext" id="infopopup">test text</span>')
-    , render_kw={"placeholder": "Name of organism"})
-    protname = StringField('', render_kw={"placeholder": "Name of protein"})
-    header = StringField('', render_kw={"placeholder": "Header of sequence"})
-    seq = StringField('', render_kw={"placeholder": "Sequence"})
-    eval_threshold = StringField('', render_kw={"placeholder": "E-value threshold"})
-    query_coverage = StringField('', render_kw={"placeholder": "Query coverage"})
-    origin = StringField('', render_kw={"placeholder": "Origin"})
+                                 '<span class="popuptext" id="infopopup_orgname">test text</span>')
+                          , render_kw={"placeholder": "Name of organism"})
+    protname = StringField(Markup('<button_protname class="info-button", onclick="infoPopUp_protname()", title="info">'
+                                  '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                                  '<span class="popuptext" id="infopopup_protname">test text</span>')
+                           , render_kw={"placeholder": "Name of protein"})
+    header = StringField(Markup('<button_header class="info-button", onclick="infoPopUp_header()", title="info">'
+                                '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                                '<span class="popuptext" id="infopopup_header">test text</span>')
+                         , render_kw={"placeholder": "Header of sequence"})
+    seq = StringField(Markup('<button_seq class="info-button", onclick="infoPopUp_seq()", title="info">'
+                             '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                             '<span class="popuptext" id="infopopup_seq">test text</span>')
+                      , render_kw={"placeholder": "Sequence"})
+    eval_threshold = StringField(Markup('<button_eval_threshold class="info-button", '
+                                        'onclick="infoPopUp_eval_threshold()", title="info">'
+                                        '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                                        '<span class="popuptext" id="infopopup_eval_threshold">test text</span>')
+                                 , render_kw={"placeholder": "E-value threshold"})
+    query_coverage = StringField(Markup('<button_query_coverage class="info-button", '
+                                        'onclick="infoPopUp_query_coverage()", title="info">'
+                                        '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                                        '<span class="popuptext" id="infopopup_query_coverage">test text</span>')
+                                 , render_kw={"placeholder": "Query coverage"})
+    origin = StringField(Markup('<button_origin class="info-button", onclick="infoPopUp_origin()", title="info">'
+                                '<img src = "../static/image/info_FILL0_wght400_GRAD0_opsz48.png">'
+                                '<span class="popuptext" id="infopopup_origin">test text</span>')
+                         , render_kw={"placeholder": "Origin"})
     submit = SubmitField('Search', render_kw={"class": "btn btn-primary"})
--- static/js/popup.js
+++ static/js/popup.js
@@ -1,2 +1,20 @@
-function infoPopUp() {var popup = document.getElementById("infopopup");
+function infoPopUp_orgname() {var popup = document.getElementById("infopopup_orgname");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_protname() {var popup = document.getElementById("infopopup_protname");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_header() {var popup = document.getElementById("infopopup_header");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_seq() {var popup = document.getElementById("infopopup_seq");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_eval_threshold() {var popup = document.getElementById("infopopup_eval_threshold");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_query_coverage() {var popup = document.getElementById("infopopup_query_coverage");
+                                 popup.classList.toggle("show");}
+
+function infoPopUp_origin() {var popup = document.getElementById("infopopup_origin");
                                  popup.classList.toggle("show");}
