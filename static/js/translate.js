091e12a497940b8d962e644f89c61a475206627b
DBerkey <DAW.berkeij@student.han.nl>
2023-05-26 16:43:50 +0200

    error translate

---
+++ static/js/translate.js
@@ -0,0 +1,5 @@
+function translate() {
+    var show_en = document.getElementById("English");
+    show_en.classList.toggle("show");
+    var hide_bi = document.getElementById("Binary");
+    hide_bi.classList.toggle("hide");}
