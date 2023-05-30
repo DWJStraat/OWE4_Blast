34f2e7cffbb57a73dc510859409ea0141522aba2
DBerkey <DAW.berkeij@student.han.nl>
2023-05-30 15:35:09 +0200

    e val en q cover deleted

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,30 +1,17 @@
-function infoPopUp_orgname() {hide_protname();hide_header();hide_seq();hide_eval_threshold();
-    hide_query_coverage();
+function infoPopUp_orgname() {hide_protname();hide_header();hide_seq();
     var popup = document.getElementById("infopopup_orgname");
     popup.classList.toggle("show");}
 
-function infoPopUp_protname() {hide_orgname();hide_header();hide_seq();hide_eval_threshold();
-    hide_query_coverage();
+function infoPopUp_protname() {hide_orgname();hide_header();hide_seq();
     var popup = document.getElementById("infopopup_protname");
     popup.classList.toggle("show");}
 
-function infoPopUp_header() {hide_orgname();hide_protname();hide_seq();hide_eval_threshold();
-    hide_query_coverage();
+function infoPopUp_header() {hide_orgname();hide_protname();hide_seq();
     var popup = document.getElementById("infopopup_header");
     popup.classList.toggle("show");}
 
-function infoPopUp_seq() {hide_orgname();hide_protname();hide_header();hide_eval_threshold();
-    hide_query_coverage();
+function infoPopUp_seq() {hide_orgname();hide_protname();hide_header();
     var popup = document.getElementById("infopopup_seq");
-    popup.classList.toggle("show");}
-
-function infoPopUp_eval_threshold() {hide_orgname();hide_protname();hide_header();hide_seq();
-    hide_query_coverage();
-    var popup = document.getElementById("infopopup_eval_threshold");
-    popup.classList.toggle("show");}
-
-function infoPopUp_query_coverage() {hide_orgname();hide_protname();hide_header();hide_seq();hide_eval_threshold();
-    var popup = document.getElementById("infopopup_query_coverage");
     popup.classList.toggle("show");}
 
 function hide_orgname() {var popup = document.getElementById("infopopup_orgname");
@@ -35,8 +22,4 @@ function hide_protname() {var popup = document.getElementById("infopopup_protnam
 function hide_header() {var popup = document.getElementById("infopopup_header");
     popup.classList.remove("show");}
 function hide_seq() {var popup = document.getElementById("infopopup_seq");
-    popup.classList.remove("show");}
-function hide_eval_threshold() {var popup = document.getElementById("infopopup_eval_threshold");
-    popup.classList.remove("show");}
-function hide_query_coverage() {var popup = document.getElementById("infopopup_query_coverage");
     popup.classList.remove("show");}
